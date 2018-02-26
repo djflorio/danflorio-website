ff9fca30bea4bba3a8ee18befdb509fee7d56dc8
Dan Florio <dflo@danflorio.com>
2018-02-26 16:24:27 -0500

    update about

--- src/parts/about/About.js
+++ src/parts/about/About.js
@@ -4,14 +4,33 @@ import './About.css';
 import { real, fake } from '../portfolio/PortfolioItems';
 import Portfolio from '../portfolio/Portfolio';
 
-const About = () => {
+class About extends React.Component {
 
-  return (
-    <section className="about">
-      <Portfolio items={real} />
-      <Portfolio items={fake} />
-    </section>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      verb: "create things with React"
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      verb: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <section className="about">
+        <h1 className="about__header">I like to {this.state.verb}.</h1>
+        <h3 className="about__subheader">But I can</h3>
+        <input className="about__input" type="text" onChange={this.onChange} placeholder="do whatever you need me to do" />
+        <h3 className="about__subheader">.</h3>
+      </section>
+    );
+  }
 }
 
 export default About;
