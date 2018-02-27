3447a6ea018661eebdf49d4a059eaf401b8dc841
Dan Florio <dflo@danflorio.com>
2018-02-27 08:34:38 -0500

    add paralax for portfolio

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ import About from './parts/about/About';
 import Portfolio from './parts/portfolio/Portfolio';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gearShift: 50,
+      bracketShift: -850
+    }
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(e) {
+    let scrollTop = e.target.scrollingElement.scrollTop;
+    let gearTranslate = 300 - (400 * scrollTop) / 600;
+    let bracketTranslate = -850 + (200 * scrollTop) / 200;
+
+    this.setState({
+      gearShift: gearTranslate,
+      bracketShift: bracketTranslate
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Hero />
-        <About />
-        <Portfolio />
+        <About gearShift={this.state.gearShift}/>
+        <Portfolio bracketShift={this.state.bracketShift}/>
       </div>
     );
   }
--- src/parts/about/About.js
+++ src/parts/about/About.js
@@ -4,52 +4,23 @@ import './About.css';
 import { real, fake } from '../portfolio/PortfolioItems';
 import Portfolio from '../portfolio/Portfolio';
 
-class About extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      gearY: 50
-    }
-
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll(e) {
-    let scrollTop = e.target.scrollingElement.scrollTop;
-    let itemTranslate = 300 - (400 * scrollTop) / 600;
-
-    this.setState({
-      gearY: itemTranslate
-    });
-  }
-
-  render() {
-    return (
-      <section className="about" style={{ backgroundPosition: `50px ${this.state.gearY}px` }}>
-        <div className="about__section-1">
-          <h1 className="about__header">I like to create things with...</h1>
-          <h2 className="about__subheader">
-            Javascript, PHP, Python, and Java
-          </h2>
-        </div>
-        <div className="about__section-2">
-          <h1 className="about__header">...using tools like...</h1>
-          <h2 className="about__subheader">
-            React, Redux, JQuery, Angular, Wordpress, Django, LibGDX
-          </h2>
-        </div>
-      </section>
-    );
-  }
+const About = ({gearShift}) => {
+  return (
+    <section className="about" style={{ backgroundPosition: `50px ${gearShift}px` }}>
+      <div className="about__section-1">
+        <h1 className="about__header">I like to create things with...</h1>
+        <h2 className="about__subheader">
+          Javascript, PHP, Python, and Java
+        </h2>
+      </div>
+      <div className="about__section-2">
+        <h1 className="about__header">...using tools like...</h1>
+        <h2 className="about__subheader">
+          React, Redux, JQuery, Angular, Wordpress, Django, LibGDX
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default About;
